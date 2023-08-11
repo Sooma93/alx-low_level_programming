3f6d13b3d778c0ed0475d4eb9df6b6b0bc03f096
sooma93 <tasneemabasit5@gmail.com>
2023-08-11 10:53:15 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entery level
+ * descrption: numbers on base 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 0; m <= 9; m++)
+	{
+		printf("%d", m);
+	}
+	printf("\n");
+		return (0);
+}
