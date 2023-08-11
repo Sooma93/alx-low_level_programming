10b7f9e9a5d8e13cbd462bc06b464e393f18b149
sooma93 <tasneemabasit5@gmail.com>
2023-08-11 11:37:26 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ * Descrption: alphber reversed
+ * Return: Always 0
+ */
+int main(void)
+{
+	int m = 122;
+
+	while (m >= 97)
+	{
+		putchar(m);
+		m--;
+	}
+	putchar('\n');
+		return (0);
+}
