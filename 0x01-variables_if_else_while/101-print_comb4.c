d2707a3fef0021164ac1d6f12feeeb3334c58e96
sooma93 <tasneemabasit5@gmail.com>
2023-08-13 10:00:16 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,28 +6,30 @@
  */
 int main(void)
 {
-	int num1;
-	int num2;
-	int num3;
+	int a;
+	int b;
+	int c;
+	int z = 0;
 
-	for (num1 = '0'; num1 < '9'; num1++)
+	for (a = '0'; a <= '7'; a++)
 	{
-		for (num2 = num1 + 1; num2 <= '9'; num2++)
+		for (b = '0'; num2 <= '8'; b++)
 		{
-			for (num3 = num2 + 1; num3 <= 9; num3++)
+			for (c = '0'; c <= '9'; c++)
 			{
-				if ((num2 != num1) != num3)
+				if (a < b && b <= c && a != b && b != c)
 				{
-					putchar(num1);
-					putchar(num2);
-					putchar(num3);
-					if (num1 == '7' && num2 == '8')
-						continue;
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					z++
+					if (z < 126)
 					putchar(',');
 						putchar(' ');
 				}
 			}
 		}
+		z++
 	}
 	putchar('\n');
 		return (0);
