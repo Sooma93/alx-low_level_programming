2e236362750fc84bc67ef35fae9ba2e16b65e22b
sooma93 <tasneemabasit5@gmail.com>
2023-08-13 14:38:15 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,34 +1,36 @@
 #include <stdio.h>
 /**
- * main - Entery pointer
- * Descrption: three digits
+ * main - Entery point
+ * Descrption: theree digits
  * Return: Always 0
  */
 int main(void)
 {
-        int num1;
-        int num2;
-        int num3;
+	int num1 = 0;
+	int num2, num3;
 
-        for (num1 = '0'; num1 < '9'; num1++)
-        {
-                for (num2 = num1 + 1; num2 <= '9'; num2++)
-                {
-                        for (num3 = num2 + 1; num3 <= 9; num3++)
-                        {
-                                if ((num2 != num1) != num3)
-                                {
-                                        putchar(num1);
-                                        putchar(num2);
-                                        putchar(num3);
-                                        if (num1 == '7' && num2 == '8')
-                                                continue;
-                                        putchar(',');
-                                                putchar(' ');
-                                }
-                        }
-                }
-        }
-        putchar('\n');
-                return (0);
-}
+	while (num1 <= 9)
+	{
+		num2 = 0;
+		while (num2 <= 9)
+		{
+			num3 = 0;
+			while (num3 <= 9)
+			{
+				if (num1 != num2 && num1 < num2 && num2 != num3 && num2 < num3)
+				{
+					putchar(num1 + 48);
+					putchar(num2 + 48);
+					putchar(num3 + 48);
+					if (num1 + num2 + num3 != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+		putchar('\n');
+		return (0);
+	}
