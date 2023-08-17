5ee8c19669f954b64050ec79562949ebe6e55155
sooma93 <tasneemabasit5@gmail.com>
2023-08-17 12:08:29 -0700

    taz

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,17 +7,22 @@
 void print_diagonal(int n)
 {
 	if (n <= 0)
+	{
 		_putchar('\n');
-	else
+	} else
 	{
 		int i, j;
 
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j <= i; j++)
-				_putchar(' ');
-			_putchar('\\');
-			_putchar('\n');
+			for (j = 0; j < n; j++)
+			{
+			if (j == i)
+				_putchar('\\');
+			else if (j < i)
+			_putchar(' ');
 		}
+		_putchar('\n');
 	}
+}
 }
