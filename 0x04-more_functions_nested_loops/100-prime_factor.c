d9dbd7d2530774fb1314d2bd0549d2e22624cf65
sooma93 <tasneemabasit5@gmail.com>
2023-08-17 13:59:44 -0700

    hmz

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - print the largest number
+ * Return: 0
+ */
+int main(void)
+{
+	long x, max;
+	long number = 612852475143;
+	double square = sqrt(number);
+
+	for (x = 1; x <= square; x++)
+	{
+	if (number % x == 0)
+	{
+	max = number / x;
+	}
+	}
+	printf("%ld\n", max);
+	return (0);
+}
