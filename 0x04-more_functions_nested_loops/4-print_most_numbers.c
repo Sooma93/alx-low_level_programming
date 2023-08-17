b856c46e4843261fd88fec9e70fde06b931ea8ad
sooma93 <tasneemabasit5@gmail.com>
2023-08-17 10:43:05 -0700

    print most number

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_number - numbers feom 0 to 9 exapt 2 and 4
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
