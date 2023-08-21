05d32523a4db99bb19388e0adab5c68f66916991
sooma93 <tasneemabasit5@gmail.com>
2023-08-21 14:37:20 -0700

    last half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,12 +7,12 @@
 void puts_half(char *str)
 {
 	int i, n;
-	int count;
+	int count = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
 		count++;
 	n = (count - 1) / 2;
-		for (i = n + 1; i != '\0'; i++)
+		for (i = n + 1; str[i] != '\0'; i++)
 			_putchar(str[i]);
 	_putchar('\n');
 }
