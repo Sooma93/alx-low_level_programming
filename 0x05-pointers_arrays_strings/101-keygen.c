710874dcb100cf848d38c71b6a263193ab44dc49
sooma93 <tasneemabasit5@gmail.com>
2023-08-21 16:04:35 -0700

    some

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	int pass[100]
+	int pass[100];
 		int i, sum, n;
 
 	sum = 0;
@@ -21,7 +21,7 @@ int main(void)
 		putchar(pass[i] + '0');
 		if ((2772 - sum) - '0' < 78)
 		{
-			n = 2772 - sum - '0'
+			n = 2772 - sum - '0';
 				sum += n;
 			putchar(n + '0');
 			break;
