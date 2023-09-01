f9a2c3b95a892c8a6fc304aedb2d7ee78f9deda2
sooma93 <tasneemabasit5@gmail.com>
2023-09-01 11:04:02 -0700

    argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print the name
+ * @argc: arrguments
+ * @argv: argumients
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
