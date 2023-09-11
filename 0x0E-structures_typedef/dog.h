29851041fd94c65aedd7e9e6377d5ecc55fc1deb
sooma93 <tasneemabasit5@gmail.com>
2023-09-11 10:50:08 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - init dog str
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: the owner
+ */
+struct dog
+{
+	char *name;
+		float age;
+		char *owner;
+};
+
+#endif
