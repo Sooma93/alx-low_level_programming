1963fbfb31366b6f4698e150e879dbe82d58a655
sooma93 <tasneemabasit5@gmail.com>
2023-09-13 10:09:33 -0700

    m

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * get_op_func - correct function
  * @s: operator
@@ -17,7 +18,10 @@ int(*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (i < 5)
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
 	i++;
-	return (ops[i].f);
+	return (0);
 }
+
