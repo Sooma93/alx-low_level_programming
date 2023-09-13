a5be8b4577c9a9728193075b77479e233f88f842
sooma93 <tasneemabasit5@gmail.com>
2023-09-13 11:33:48 -0700

    last

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
-#include <string.h>
+
 /**
  * get_op_func - correct function
  * @s: operator
@@ -14,14 +14,12 @@ int(*get_op_func(char *s))(int, int)
 	{"*", op_mul},
 	{"/", op_div},
 	{"%", op_mod},
-	{NULL, NULL}
+	{NULL, NULL},
 	};
 	int i = 0;
 
-	while (i < 5)
-		if (strcmp(s, ops[i].op) == 0)
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
 			return (ops[i].f);
-	i++;
-	return (0);
 }
 
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,3 +1,4 @@
+#include "function_pointers.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include "3-calc.h"
@@ -7,22 +8,30 @@
  * @argv: array
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__))argc, char *argv[])
 {
-	int (*oprt)(int, int);
+	int num1, num2;
+	char *op;
 
-		if (argc != 4)
-		{
-			printf("ERROR\n");
-				exit(98);
-		}
-		oprt = get_op_func(argv[2]);
-
-	if (!oprt)
+	if (argc != 4)
+	{
+		printf("ERROR\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("ERROR\n");
 		exit(99);
 	}
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
-	return (0);
+	if ((*op == '/' && num2 == 0) ||
+			(*op == '%' && num2 == 0))
+	{
+		printf("ERROR\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
+	return(0);
 }
