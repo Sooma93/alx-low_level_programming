d6b904bcaf3c1620ddadae57d28b24b30e7abae4
sooma93 <tasneemabasit5@gmail.com>
2023-09-13 21:08:37 -0700

    m

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,30 +8,20 @@
  */
 int main(int argc, char *argv[])
 {
-	int bytes, i;
-	char *arry;
+	int i;
 
 	if (argc != 2)
 	{
 		printf("ERROR\n");
 		exit(1);
 	}
-	bytes = atoi(argv[1]);
-
-	if (bytes < 0)
+	if (atoi(argv[1]) < 0)
 	{
 		printf("ERROR\n");
 		exit(2);
 	}
-	arry = (char *)main;
-	for (i = 0; i < bytes; i++)
-	{
-		if (i == bytes - 1)
-		{
-		printf("%02hhx\n", arry[i]);
-		break;
-	}
-	printf("%02hhx", arry[i]);
-	}
+	for (i = 0; i < atoi(argv[1]) - 1; i++)
+		printf("%02hhx\n", ((char *)main)[i]);
+	printf("%02hhx\n", ((char *)main)[i]);
 	return (0);
 }
