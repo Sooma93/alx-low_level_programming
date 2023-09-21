3e4a0ab1c8594c342636677ade5b3d2813313cfe
sooma93 <tasneemabasit5@gmail.com>
2023-09-21 15:55:33 -0700

    roz

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void firts(void) __attribute__ ((constructor));
+void __attribute__ ((constructor))first (void);
 /**
  * first - print before main func
  */
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
