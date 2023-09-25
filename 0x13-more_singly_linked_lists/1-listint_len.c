fc8e9e257a1003b4a106402c68e6bf9278ad9237
sooma93 <tasneemabasit5@gmail.com>
2023-09-25 12:58:12 -0700

    ele

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * listint_len - return nodes
+ * @h: link list
+ * Return: number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
