e1d348d14576de84c86b3cf6a1f587aee1c41e56
sooma93 <tasneemabasit5@gmail.com>
2023-09-28 14:32:14 -0700

    sooz

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * clear_bit - set value of 0
+ * @n: pointer
+ * @index: index value
+ * Return: 1 or -1
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i;
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	i = ~(1 << index);
+	*n = *n & i;
+	return (1);
+}
